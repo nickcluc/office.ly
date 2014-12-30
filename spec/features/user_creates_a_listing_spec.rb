ca16a76af403b40793fb54b5fd111a9e071e261b
nickcluc <nclucas01@gmail.com>
2014-12-30 00:28:16 -0500

    Initial COmmit

---
+++ spec/features/user_creates_a_listing_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+feature 'User creates a listing', %Q{
+  As a user
+  I want to Log In and create a Listing
+  So I can rent out my desk when I am on vacation
+
+  Acceptance Criteria:
+
+  [ ] A user must enter a title, address, city, state, and weekly rate
+  [ ] A user can optionally add a description
+  [ ] When successfully listed, I should see a success message & my listing
+  [ ] When listing is unsuccessful, I should see an error message
+  [ ] A user must be logged in to list a desk, if not logged in, should see an error
+} do
+
+  before :each do
+    user = FactoryGirl.create(:user)
+
+    visit new_user_registration_path
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Password confirmation', with: user.password
+
+    click_button 'Sign up'
+
+    visit new_user_session_path
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+
+    click_button 'Log in'
+  end
+
+  scenario 'User Logs In & Creates A Listing' do
+
+    listing = FactoryGirl.build(:listing)
+
+    visit new_listing_path
+
+    expect(page).to have_content "Create New Listing"
+
+    fill_in 'Title', with: listing.title
+    fill_in 'Address', with: listing.address
+    fill_in 'City', with: listing.city
+    fill_in 'State', with: listing.state
+    fill_in 'Zip code', with: listing.zip_code
+    fill_in 'Description', with: listing.description
+    fill_in 'Weekly Rate - USD', with: listing.rate_cents
+    fill_in 'Created by', with: @current_user.id
+
+    click_button "Create Listing"
+
+    expect(page).to have_content "Listing Created Successfully!"
+  end
+
+end
---
+++ spec/support/factory_girl.rb
@@ -0,0 +1,19 @@
+require 'factory_girl'
+
+FactoryGirl.define do
+  factory :user do
+    sequence(:email) {|n| "user#{n}@example.com" }
+    password 'password'
+    password_confirmation 'password'
+  end
+
+  factory :listing do
+    title 'Desk @ Launch Academy'
+    description 'Great Seat Near The Coffee Machine!!!'
+    address '33 Harrison Ave. Suite 501'
+    city 'Boston'
+    state 'MA'
+    zip_code '02111'
+    rate_cents 100
+  end
+end
