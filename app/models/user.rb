2cfb512c8b07a93475a83b0ebc3c12ba53349b83
nickcluc <nclucas01@gmail.com>
2015-01-05 23:17:02 -0500

    Added user & listing associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
   include Gravtastic
   gravtastic
   has_many :listings
-  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
