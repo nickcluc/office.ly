f55fba141f3bcb4561bb966e07b5e220da13bbca
nickcluc <nclucas01@gmail.com>
2015-01-11 19:27:43 -0500

    map moves dynamically

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150110154606) do
+ActiveRecord::Schema.define(version: 20150111205729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20150110154606) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "header_image"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "users", force: :cascade do |t|
