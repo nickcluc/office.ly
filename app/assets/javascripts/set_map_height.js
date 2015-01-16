dee2bbd4f167924959f13700fd3ac4698b3ea4b8
nickcluc <nclucas01@gmail.com>
2015-01-16 13:53:45 -0500

    geolocation?

--- app/assets/javascripts/set_map_height.js
+++ app/assets/javascripts/set_map_height.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
   var height = ($( ".info-left" ).height());
-  var priceHeight = ($("#price").height())
+  var priceHeight = ($("#price").height());
   $(".info-right" ).height(height-priceHeight);
+  var navHeight = ($("#nav-bar").height());
+  $( window ).resize(function() {
+    var windowHeight = $(window).height();
+    $("#map-canvas" ).height(windowHeight-navHeight);
+  });
 });
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,7 @@
 class HomesController < ApplicationController
   def index
     @featured = Listing.all.sample
+    @current_location = request.location
+    binding.pry
   end
 end
